a63d3f8a5c6d1db0a31aec8c65c910127b79181e
MastProTech <uzairarif1024@gmail.com>
2020-12-02 20:23:44 +0500

    Completed Challenge 2020/1

---
+++ 2020/01.py
@@ -0,0 +1,37 @@
+import re
+def read_file(path):
+    fh=open(str(path))
+    text=str()
+    for line in fh:
+        text=text+line
+    return text
+
+def text_to_list(text:str):
+    num_list=list()
+    pattern=re.compile('[0-9]+')
+    matches=pattern.finditer(text)
+    for match in matches:
+        num_list.append(int(match.group(0)))
+    return num_list
+
+def part1(num_list:list, target:int):
+    for i in range(len(num_list)):
+        for j in range(i+1, len(num_list)):
+            sum=num_list[i]+num_list[j]
+            if sum==target:
+                mul=num_list[i]*num_list[j]
+                print('YES',i, 'and', j, 'mul:',mul)
+
+def part2(num_list:list, target:int):
+    for i in range(len(num_list)):
+        for j in range(i+1, len(num_list)):
+            for k in range(j+1, len(num_list)):
+                sum=num_list[i]+num_list[j]+num_list[k]
+                if sum==target:
+                    mul=num_list[i]*num_list[j]*num_list[k]
+                    print('YES',i, 'and', j, 'k', k, 'mul:',mul)
+
+text=read_file('01.txt')
+num_list=text_to_list(text)
+part1(num_list, 2020)
+part2(num_list, 2020)
