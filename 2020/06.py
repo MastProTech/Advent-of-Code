7ab6a711b4a8a76e44e03b21a4c61f3df63b4bc6
MastProTech <uzairarif1024@gmail.com>
2020-12-09 22:42:18 +0500

    Updated 06.py

--- 2020/06.py
+++ 2020/06.py
@@ -21,9 +21,12 @@ def part2(l:list):
     for i in l:
         questions=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
         for j in questions:
+            # Each \n indicates a new person. So, I count('\n')+1=count(persons)
+            # Plus, for each letter, check number of repetitions of that letter and compare it with no. of persons in that group.
             if len(re.findall(j, i))==(len(re.findall('\n', i))+1):
                 count+=1
     return count
+
 answers=read_file('06.txt')
 print('Part 1: Number of Questions answered YES:', part1(answers))
-print('Part2:',part2(answers))
+print('Part 2: Number of Questions everyone in group answered YES:',part2(answers))
